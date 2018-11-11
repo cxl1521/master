846171c23c1ada21626c735182fa7188be099b27
user <user@LAPTOP-EC8Q5CQ9>
2018-11-11 17:56:25 +0800

    add one more assertion

--- TriType/src/TestTriType.java
+++ TriType/src/TestTriType.java
@@ -19,5 +19,6 @@ public class TestTriType {
 		assertEquals(t.Tritype(3, 3, 4), 1);
 		assertEquals(t.Tritype(3, 3, 3), 2);
 		assertEquals(t.Tritype(3, 3, -1), 3);
+		assertEquals(t.Tritype(-3, 3, -1), 3);
 	}
 }
